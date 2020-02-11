a0e6e36c183302d552734f442b4241349f65221c
RyuZUv <31495025+RyuZUv@users.noreply.github.com>
2020-02-11 12:45:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,5 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git
+git clone https://github.com/tty228/luci-app-serverchan.git
 ./scripts/feeds update -a
 ./scripts/feeds install -a
